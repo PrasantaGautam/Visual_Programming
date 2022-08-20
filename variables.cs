22deadad0a4a1821e7321d52a39cca901892dc17
Prashant Gautam <prashantgautam000000@gmail.com>
2022-08-20 21:14:54 +0545

    variables declaration

---
+++ variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    	int myNum = 5;
+		double myDoubleNum = 5.99D;
+		char myLetter = 'D';
+		bool myBool = true;
+		string myText = "Hello";
+    }
+  }
+}
