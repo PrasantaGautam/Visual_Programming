fd39da6fa7c48f04f038ed4cf133d912a1b8735c
Prashant Gautam <prashantgautam000000@gmail.com>
2022-08-22 22:01:31 +0545

    datatypes

---
+++ datatypes.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Here we have many type of datatypes like:
+        //bool, byte, short, ushort, int, uint, long, ulong, etc..
+        //Some of the datatypes are used below to store their types data/values
+        //datatype variable_name = data/value
+        bool boolVar = true;
+        byte byteVar = 255;
+        short shortVar = -66;
+        ushort ushortVar = 66;
+        int intVar = -1000;
+        uint uintVar = 1000;
+        long longVar = -56000;
+        ulong ulongVar = 56000;
+        float floatVar = 160.54 F;
+        double doubleVar = 13782.57543 D;
+        char charVar = 'A';
+    }
+}
