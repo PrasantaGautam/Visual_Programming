988662cf542007fe005c85a20de6fb8f13248409
Prashant Gautam <prashantgautam000000@gmail.com>
2022-08-22 22:00:26 +0545

    \updated

--- variables.cs
+++ variables.cs
@@ -6,11 +6,15 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
+    // Variables are the name given to the memory location of data
+    	// Here int, double, char, etc are the datatypes and name after the datatypes are variables
+    	//datatype variable_name = data/value
     	int myNum = 5;
-		double myDoubleNum = 5.99D;
-		char myLetter = 'D';
-		bool myBool = true;
-		string myText = "Hello";
+	double myDoubleNum = 5.99D;
+	char myLetter = 'D';
+	bool myBool = true;
+	string myText = "Hello";
+	// We can write variable name as our choice 
     }
   }
 }
