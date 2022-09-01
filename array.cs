23dfa213093ca81c44154557d7bbdad406204a12
Prashant Gautam <86648752+PrasantaGautam@users.noreply.github.com>
2022-09-01 21:59:07 +0545

    Array

---
+++ array.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Array index starts from 0 
+      string[] cars = {"One", "Two", "Three", "Four"};
+      Console.WriteLine(cars[0]);
+    }
+  }
+}
