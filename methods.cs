a7ab9822f671dbb026505f6ebdde4c29787c02cc
Prashant Gautam <86648752+PrasantaGautam@users.noreply.github.com>
2022-09-02 22:05:37 +0545

    Methods

---
+++ methods.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyProgram
+{
+  class Program
+  {
+    // Functions inside class are called methods
+    static void MyFun()
+    {
+      Console.WriteLine("Function just executed !");
+    }
+
+    static void Main(string[] args)
+    {
+      MyFun();
+    }
+  }
+}
