b8149e6057a8fc6b38b68e1264858d6e0c1daec6
Prashant Gautam <86648752+PrasantaGautam@users.noreply.github.com>
2022-09-04 21:32:57 +0545

    Flag of Nepal

---
+++ question_4.cs
@@ -0,0 +1,31 @@
+// Question : Make a Falg of Nepal using asterik (*)
+*
+**
+***
+****
+*****
+*
+**
+***
+****
+*****
+
+using System;
+
+public class HelloWorld 
+{
+	public static void Main(string[] args) 
+	{	
+		for(int i=0;i<2;i++) 
+		{
+			for(int j=0;j<5;j++)
+			{ 
+				for(int k=0;k<=j;k++) 
+				{	
+					Console.Write("*"); 
+				}
+ 			Console.Write("\n");
+			}
+		}
+	}
+}
